dcb31da047341d321808bbbf84cc03f4c4dc2631
zhangqiang <2467558421@qq.com>
2024-05-30 17:33:47 +0800

    调整默认值

--- src/store/appUserInfo.js
+++ src/store/appUserInfo.js
@@ -9,9 +9,48 @@ export const appUserInfo = defineStore("appUserInfo", {
   state: () => ({
     // eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzb20uempscm1vdmllLmNuIiwiYXVkIjoic29tLnpqbHJtb3ZpZS5jbiIsImlhdCI6MTcxNTM0NDk2MywibmJmIjoxNzE1MzQ0OTYzLCJleHAiOjE3MTc3NjQxNjMsImRhdGEiOnsiaWQiOjcxNDYzMiwidXNlcm5hbWUiOjcxNDYzMiwic3RhdHVzIjoxLCJvcGVuaWQiOiJvUXpFZjQ3a1ZLQ3F6bzRPSXl1ZHBZVllwX2g0In19.mwidYdjsGHIEnDxWlihB2LVdCtt0o1v_rrdbvSbSe50
     allUserInfo: allUserInfo || {
-      sfc: null,
-      jiujin: null,
-      jinji: null,
+      sfc: {
+        card_count: "0",
+        card_num: "0",
+        have_new_coupon: "0",
+        head_img: "",
+        mobile: "13073795001",
+        nickname: "13073795001",
+        no_pay_order: "0",
+        session_id: "6655c6d5bbc32f90bef1d9a9e3eb972d85bee9af93416",
+        user_id: "581154",
+        modified_birthday: "0",
+        modified_info: "0",
+        member_center_h5_url: "http://group.leying.com/point/member-center"
+      },
+      jiujin: {
+        card_count: "1",
+        card_num: "1",
+        have_new_coupon: "0",
+        head_img: "",
+        mobile: "13073792313",
+        nickname: "13073792313",
+        no_pay_order: "0",
+        session_id: "66557ba8dd06c6d03ca807ff9fb6bff797c0b325138e8",
+        user_id: "2482",
+        modified_birthday: "0",
+        modified_info: "0",
+        member_center_h5_url: "http://group.leying.com/point/member-center"
+      },
+      jinji: {
+        card_count: "2",
+        card_num: "2",
+        have_new_coupon: "0",
+        head_img: "",
+        mobile: "13937705167",
+        nickname: "13937705167",
+        no_pay_order: "0",
+        session_id: "6655d8957f109fb75979e960f0d63e618653254b15cc9",
+        user_id: "10298",
+        modified_birthday: "0",
+        modified_info: "0",
+        member_center_h5_url: "http://group.leying.com/point/member-center"
+      },
       laina: null
     }
   }),
--- src/store/offerRule.js
+++ src/store/offerRule.js
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
-
+import { OFFER_LIST } from "@/common/constant";
 export const useDataTableStore = defineStore("dataTable", {
   state: () => {
     return {
@@ -18,139 +18,7 @@ export const useDataTableStore = defineStore("dataTable", {
           return;
         }
       }
-      this.items = [
-        {
-          id: 1,
-          orderForm: "lieren",
-          ruleName: "1线城市普厅",
-          shadowLineName: "sfc",
-          status: "1",
-          isOffer: "1",
-          offerType: "1",
-          offerAmount: "40.5",
-          includeCityNames: ["北京市", "上海市", "杭州市"],
-          excludeCityNames: [],
-          includeCinemaNames: [],
-          excludeCinemaNames: [],
-          includeHallNames: [],
-          excludeHallNames: [],
-          includeFilmNames: [],
-          excludeFilmNames: [],
-          weekDay: [],
-          memberDay: "",
-          timeLimit: "",
-          seatNum: "",
-          ruleStartTime: "07:00",
-          ruleEndTime: "18:00"
-        },
-        {
-          id: 2,
-          orderForm: "lieren",
-          ruleName: "2线城市普厅",
-          shadowLineName: "sfc",
-          status: "1",
-          isOffer: "1",
-          offerType: "1",
-          offerAmount: "37",
-          includeCityNames: [],
-          excludeCityNames: ["北京市", "上海市", "杭州市"],
-          includeCinemaNames: [],
-          excludeCinemaNames: [],
-          includeHallNames: [],
-          excludeHallNames: [],
-          includeFilmNames: [],
-          excludeFilmNames: [],
-          weekDay: ["星期一", "星期二"],
-          memberDay: "",
-          timeLimit: "",
-          seatNum: "1",
-          ruleStartTime: "07:00",
-          ruleEndTime: "18:00"
-        },
-        {
-          id: 3,
-          orderForm: "lieren",
-          ruleName: "特殊厅报价",
-          shadowLineName: "sfc",
-          status: "1",
-          isOffer: "1",
-          offerType: "2",
-          addAmount: "2",
-          includeCityNames: [],
-          excludeCityNames: [],
-          includeCinemaNames: [],
-          excludeCinemaNames: [],
-          includeHallNames: [
-            "vip",
-            "4D",
-            "IMAX",
-            "巨幕",
-            "Onyx LED",
-            "Atmos",
-            "4DX",
-            "LUXE",
-            "Cinema",
-            "LED",
-            "杜比",
-            "元宇宙"
-          ],
-          excludeHallNames: [],
-          includeFilmNames: [],
-          excludeFilmNames: [],
-          weekDay: [],
-          memberDay: "",
-          timeLimit: "6",
-          seatNum: "2",
-          ruleStartTime: "07:00",
-          ruleEndTime: "18:00"
-        },
-        {
-          id: 4,
-          orderForm: "lieren",
-          ruleName: "会员日报价-普通厅",
-          shadowLineName: "sfc",
-          status: "1",
-          isOffer: "1",
-          offerType: "3",
-          offerAmount: "20",
-          includeCityNames: [],
-          excludeCityNames: [],
-          includeCinemaNames: [],
-          excludeCinemaNames: [],
-          includeHallNames: [],
-          excludeHallNames: [],
-          includeFilmNames: [],
-          excludeFilmNames: [],
-          weekDay: [],
-          memberDay: "2024-05-12",
-          timeLimit: "",
-          ruleStartTime: "07:00",
-          ruleEndTime: "18:00"
-        },
-        {
-          id: 5,
-          orderForm: "lieren",
-          ruleName: "会员日报价-特殊厅",
-          shadowLineName: "sfc",
-          status: "1",
-          isOffer: "1",
-          offerType: "3",
-          offerAmount: "30",
-          includeCityNames: [],
-          excludeCityNames: [],
-          includeCinemaNames: [],
-          excludeCinemaNames: [],
-          includeHallNames: ["VIP厅"],
-          excludeHallNames: [],
-          includeFilmNames: [],
-          excludeFilmNames: [],
-          weekDay: [],
-          memberDay: "2024-05-12",
-          timeLimit: "",
-          ruleStartTime: "07:00",
-          ruleEndTime: "18:00"
-        }
-      ];
+      this.items = OFFER_LIST;
     },
     addItem(item) {
       this.items.push(item);
