39b10012039fc35f2743ef5b18aabd54eaae8730
张强 <2467558421@qq.com>
2025-02-17 20:15:37 +0800

    调整外部账号不能看到特殊匹配列表

--- src/router/utils.ts
+++ src/router/utils.ts
@@ -213,7 +213,7 @@ function initRouter(rule: any) {
     return new Promise(resolve => {
       getAsyncRoutes().then(({ data }) => {
         console.log("data0", data);
-        if (![3].includes(rule)) {
+        if (![3, 6].includes(rule)) {
           data?.[0]?.children.push(
             ...[
               {
