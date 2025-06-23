49d4e3ac4b7b68ed83574c0b9277fb307170cc6b
张强 <2467558421@qq.com>
2025-06-23 10:58:34 +0800

    解决ume系列报价取值异常的问题

--- src/common/autoOffer/umeOffer.js
+++ src/common/autoOffer/umeOffer.js
@@ -1629,18 +1629,37 @@ class getUmeOfferPrice {
         des: "获取座位布局相关信息",
         level: "info",
         info: {
-          areaInfoList
+          areaInfoList: JSON.parse(JSON.stringify(areaInfoList))
         }
       });
       let maxSeatPrice, mostSeatPrice;
       if (areaInfoList?.length) {
         // 座位分区从高到低排序
-        let areaList = areaInfoList.sort(
-          (a, b) =>
-            b.price + Number(b.areaServiceFee) - a.price - a.areaServiceFee
-        );
+        let areaList = areaInfoList
+          .map(item => {
+            let priceInfo;
+            if (item.areaMemberPrice?.length) {
+              priceInfo = item.areaMemberPrice.sort(
+                (a, b) => b.settlePrice - a.settlePrice
+              )[0];
+            } else {
+              priceInfo = { settlePrice: item.areaSettlePrice || 0 };
+            }
+            return {
+              ...item,
+              ...priceInfo
+            };
+          })
+          .sort(
+            (a, b) =>
+              b.settlePrice +
+              Number(b.areaServiceFee) -
+              a.settlePrice -
+              a.areaServiceFee
+          );
         // 复制座位分区最高价
-        maxSeatPrice = areaList[0].price + Number(areaList[0].areaServiceFee);
+        maxSeatPrice =
+          areaList[0].settlePrice + Number(areaList[0].areaServiceFee);
         // 获取最多座位价格
         mostSeatPrice = this.getMostSeatPrice(seat_data, areaList);
       }
@@ -1690,7 +1709,7 @@ class getUmeOfferPrice {
         }
       });
       let mostSeatPrice =
-        areaRatioList[0].price + Number(areaRatioList[0].areaServiceFee);
+        areaRatioList[0].settlePrice + Number(areaRatioList[0].areaServiceFee);
       return mostSeatPrice;
       // // 默认取最高价格，最高座位占比不足百分之3时取次最高价格
       // if (areaList[0].numRatio <= 3 && areaList[1]?.settlePrice) {
