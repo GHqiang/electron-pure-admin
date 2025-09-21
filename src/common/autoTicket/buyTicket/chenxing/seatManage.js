de6b433b70111b56915aecae020955a112bba4f8
张强 <2467558421@qq.com>
2025-09-21 14:18:56 +0800

    解决哈哈获取目标座位失败的问题

--- src/common/autoTicket/buyTicket/chenxing/seatManage.js
+++ src/common/autoTicket/buyTicket/chenxing/seatManage.js
@@ -57,7 +57,7 @@ export default class SeatManage {
       }
 
       const targetSeats = this.filterTargetSeats(seatList);
-      if (targetSeats.length !== this.order.ticket_num) {
+      if (targetSeats.length != this.order.ticket_num) {
         this.logger.errorSave("获取目标座位失败", {
           targetSeats,
           ticket_num: this.order.ticket_num
