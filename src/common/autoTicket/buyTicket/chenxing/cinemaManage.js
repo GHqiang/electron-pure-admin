a7d8bf051a729f89630f68205179622c03e8e809
张强 <2467558421@qq.com>
2025-10-01 17:35:53 +0800

    解决辰星匹配影片放映场次失败时日志异常问题

--- src/common/autoTicket/buyTicket/chenxing/cinemaManage.js
+++ src/common/autoTicket/buyTicket/chenxing/cinemaManage.js
@@ -474,7 +474,7 @@ export default class CinemaManage {
           false
         );
       }
-      this.logger.errorSave("匹配影片放映场次失败", { showLis, cinemaInfo });
+      this.logger.errorSave("匹配影片放映场次失败", { showList, cinemaInfo });
     } catch (error) {
       this.logger.errorSave("场次匹配异常", formatErrInfo(error));
     }
