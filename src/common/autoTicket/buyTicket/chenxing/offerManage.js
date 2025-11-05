c21272569fc0fe0dd5d2157ce73d6902edd4cd14
张强 <2467558421@qq.com>
2025-11-05 13:49:26 +0800

    ### 5.8.15版本 1、解决凤凰新有服务费场景会员价格读取不准确的问题；

--- src/common/autoTicket/buyTicket/chenxing/offerManage.js
+++ src/common/autoTicket/buyTicket/chenxing/offerManage.js
@@ -481,14 +481,14 @@ class getChenxingOfferPrice {
           // 取最低价
           basePrice = discountList
             .filter(item => item.cardLevelCode)
-            .map(item => item.price - item.cinemaPayAmount)
+            .map(item => item.price - item.cinemaPayAmount + item.serviceAddFee)
             .sort((a, b) => a - b)?.[0];
           this.logger.infoSave("从有卡优惠活动里取最低价", { basePrice });
-          if(!basePrice) {
+          if (!basePrice) {
             basePrice = discountList
-            .filter(item => !item.cardLevelCode)
-            .map(item => item.price - item.cinemaPayAmount)
-            .sort((a, b) => a - b)?.[0];
+              .filter(item => !item.cardLevelCode)
+              .map(item => item.price - item.cinemaPayAmount)
+              .sort((a, b) => a - b)?.[0];
             this.logger.infoSave("从无卡优惠活动里取最低价", { basePrice });
           }
         } else {
