ebad7324e4c73db241151247bf79df29dba49bb7
张强 <2467558421@qq.com>
2025-12-16 13:35:02 +0800

    调整蚂蚁商户号和密钥根据队列配置来

--- src/utils/http/mayi-request.js
+++ src/utils/http/mayi-request.js
@@ -16,14 +16,14 @@ const instance = axios.create({
 
 const NODE_ENV = process.env.NODE_ENV;
 const IS_DEV = NODE_ENV === "development";
-let merCode = "00696"; // 兜哥商户号
-let priKey = "9PX4UDEGH3NBODPNMP3LO2EBRTKSYHPW"; // 兜哥商户密钥
+let merCode = ""; // 兜哥商户号：00696
+let priKey = ""; // 兜哥商户密钥：9PX4UDEGH3NBODPNMP3LO2EBRTKSYHPW
 
 // 请求拦截器
 instance.interceptors.request.use(
   config => {
-    // priKey = localStorage.getItem("mayiPlatSubToken");
-    // merCode = localStorage.getItem("mayiPlatUserUUID");
+    priKey = localStorage.getItem("mayiPlatSubToken");
+    merCode = localStorage.getItem("mayiPlatUserUUID");
     // 检查必填配置
     if (!merCode || !priKey) {
       console.error(
