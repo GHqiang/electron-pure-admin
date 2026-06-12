115bd915ae4b78ccbbdf432c7564b65e7d8a3ff3
张强 <2467558421@qq.com>
2026-06-12 21:42:18 +0800

    ### 6.3.45版本

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -516,7 +516,7 @@ const getCinemaLoginInfoList = (isSplitUser = true) => {
       );
     }
     if (user_id == 1) {
-      // user_id = 9;
+      user_id = 9;
       // user_id = 10;
     }
     loginInfoList = loginInfoList.filter(item =>
