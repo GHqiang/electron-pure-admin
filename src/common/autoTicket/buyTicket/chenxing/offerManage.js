80ceb6d16d6b61ed87617accadde711447053db4
张强 <2467558421@qq.com>
2026-07-23 13:42:22 +0800

    修复辰星无卡优惠活动报价金额不准确的问题

--- src/common/autoTicket/buyTicket/chenxing/offerManage.js
+++ src/common/autoTicket/buyTicket/chenxing/offerManage.js
@@ -271,7 +271,9 @@ class getChenxingOfferPrice extends BaseOfferPrice {
           if (!basePrice) {
             basePrice = discountList
               .filter(item => !item.cardLevelCode)
-              .map(item => item.price - item.cinemaPayAmount)
+              .map(
+                item => item.price - item.cinemaPayAmount + item.serviceAddFee
+              )
               .sort((a, b) => a - b)?.[0];
             this.logger.infoSave("从无卡优惠活动里取最低价", { basePrice });
           }
